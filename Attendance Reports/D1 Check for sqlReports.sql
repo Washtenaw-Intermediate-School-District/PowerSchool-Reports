b0f2c15450ef9c6b54be6cdb95cea4d1dcf2ca69
Solomon Zheng <58829220+wisd-solomon@users.noreply.github.com>
2020-10-14 22:42:00 -0400

    Update D1 Check for sqlReports.sql
    Updated the coding to pull from the PSSIS_Attendance_Meeting table to reflect the enrollment school IDs and not just the home IDs.

--- Attendance Reports/D1 Check for sqlReports.sql
+++ Attendance Reports/D1 Check for sqlReports.sql
@@ -4,29 +4,27 @@ sc.name,
 chr(60) || 'a href=/admin/attendance/view/meeting.html?frn=001'|| to_char(st.dcid) || ' target=_blank>' || st.student_number || chr(60) || '/a>',
 st.lastfirst, 
 st.gender, 
-st.State_StudentNumber, 
+st.Student_Number, 
 st.grade_level, 
 CASE WHEN st.enroll_status = 0 THEN 'Active' ELSE 'Exited' END,
 to_char(st.entrydate,'mm/dd/yyyy'), 
 to_char(st.exitdate,'mm/dd/yyyy'),
-sum(CASE WHEN ac.att_code IN ('D1','RLD1') THEN 1 ELSE 0 END) "count_first_day",
-listagg (CASE WHEN ac.att_code IN ('D1','RLD1') THEN to_char(a.att_date,'mm/dd/yyyy') END , ',') WITHIN GROUP (Order By a.att_date) "att"
+sum(CASE WHEN ac.att_code IN ('D1','RLD1') THEN 1 ELSE 0 END),
+listagg (CASE WHEN ac.att_code IN ('D1','RLD1') THEN to_char(a.att_date,'mm/dd/yyyy') END , ',') WITHIN GROUP (Order By a.att_date)
 
 FROM students st
-    left JOIN (SELECT * FROM attendance WHERE attendance.att_date between '%param1%' and '%param2%') a ON (st.id=a.studentid and st.schoolid=a.schoolid)
+    left JOIN (SELECT * FROM PSSIS_Attendance_Meeting WHERE PSSIS_Attendance_Meeting.att_date between to_date('%param1%','mm/dd/yyyy') and to_date('%param2%','mm/dd/yyyy')) a ON (st.id=a.studentid)
     left JOIN attendance_code ac ON ac.id=a.attendance_codeid
     left JOIN Period pe ON a.periodid=pe.id
     LEFT JOIN Schools sc ON st.schoolid = sc.school_number
-~[if#cursel.%param5%=Yes]
-INNER JOIN ~[temp.table.current.selection:students] stusel ON stusel.dcid = st.dcid
-[/if#cursel]
+
 
 WHERE 
-    (st.exitdate IS NULL OR st.exitdate>'%param1%')
+    (st.exitdate IS NULL OR st.exitdate> to_date('%param1%','mm/dd/yyyy'))
     AND (a.att_date IS NULL OR (a.att_mode_code='ATT_ModeMeeting') )
-    AND st.grade_level>-1
-    AND st.schoolid IN (1925,1923,3000,9404,1153,1157,2988,1705,798,2062,1938)
-    AND st.schoolid like case when ~(curschoolid)=0 then '%' else '~(curschoolid)' end
+    AND st.schoolid IN (1925,1923,3000,9404,1153,1157,2988,1705,798,2062)
+    AND st.GRADE_LEVEL > -1
 
-GROUP BY st.dcid, sc.name, st.student_number, st.lastfirst, st.gender, st.grade_level,st.State_StudentNumber, st.enroll_status,st.ENTRYDATE, st.exitdate
+GROUP BY st.dcid, sc.name, st.student_number, st.lastfirst, st.gender, st.grade_level, st.State_StudentNumber, st.enroll_status, st.ENTRYDATE, st.exitdate
+
 order by sc.name, st.grade_level, st.lastfirst
